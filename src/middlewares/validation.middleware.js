f8ce5c9677259128ccd31c31f951f4017e40ced8
subrinSheikh <subrinSheikh@users.noreply.github.com>
2024-10-04 14:51:44 +0530

    modify password validator.

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -4,12 +4,12 @@ export const validateRequest = async (req, res, next) => {
     const rules = [
         body('name').notEmpty().withMessage(`Name is required`),
         body('email').notEmpty().withMessage(`Email is required`),
-        // body('password')
-        //     .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
-        //     .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
-        //     .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
-        //     .matches(/[0-9]/).withMessage('Password must contain at least one number')
-        //     .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character'),
+        body('password')
+            .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+            .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+            .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+            .matches(/[0-9]/).withMessage('Password must contain at least one number')
+            .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character'),
     ];
     // 2.run rules
     await Promise.all(rules.map(rule => rule.run(req)));
